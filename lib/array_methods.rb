d1737226c02ad32431a3ae5107d64771abe9514e
Karl Matthes <karlmatthes@gmail.com>
2019-11-02 22:49:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,7 +20,7 @@ end
 def find_min_value(array)
   min_value = array[0]
   array.length.times do |i|
-    if min_value < array[i]
+    if array[i] < min_value
       min_value = array[i]
     end
   end
